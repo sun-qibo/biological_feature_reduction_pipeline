65829fc2a8c8b76254544948717189eda272c545
sun-qibo <134395993+sun-qibo@users.noreply.github.com>
2025-08-04 18:30:49 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .biological_feature_reducer import BiologicalFeatureReducer, feature_reduction_pipeline
+
+__version__ = "0.1.0"
+__all__ = ["BiologicalFeatureReducer", "feature_reduction_pipeline"]
